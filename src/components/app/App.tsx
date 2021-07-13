1e25f8e2da3b6c53547344798deee2b3776f254d
Husan Eshonqulov <husan.eshonqulov.00@gmail.com>
2021-07-13 14:07:04 +0500

    btn test 1

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -2,11 +2,11 @@ import DigitBtn from '../digitBtn/DigitBtn';
 
 function App() {
     return (
-        <div>
+        <div className="d-flex justify-content-between">
             <DigitBtn digit={0} />
-            <DigitBtn digit={0} />
-            <DigitBtn digit={0} />
-            <DigitBtn digit={0} />
+            <DigitBtn digit={1} />
+            <DigitBtn digit={2} />
+            <DigitBtn digit={3} />
         </div>
     );
 }
