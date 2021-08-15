4ea5a356e769f6f24bfcaf23a00e33ab2af9887b
Husan Eshonqulov <husan.eshonqulov.00@gmail.com>
2021-08-15 13:42:13 +0500

    fix: fix BtnRow comp

--- src/components/digitBtn/DigitBtn.tsx
+++ src/components/digitBtn/DigitBtn.tsx
@@ -1,9 +1,16 @@
+import "./DigitBtn.css";
 import SignBtn from "../signBtn/SignBtn";
 import { DigitBtnType } from "../app/App";
 
 function DigitBtn({ digit, color, bg, clickDigit }: DigitBtnType) {
   return (
-    <div onClick={() => {console.log(digit); clickDigit(digit)}}>
+    <div
+      onClick={() => {
+        console.log(digit);
+        clickDigit(digit);
+      }}
+      className="digitBtn-div"
+    >
       <SignBtn sign={digit} color={color} bg={bg} />
     </div>
   );
